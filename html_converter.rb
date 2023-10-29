a2fac715290c2959a1861203311656ff6fc50077
RaviHelon-msf <ravihelon@gmail.com>
2023-10-28 21:11:57 -0300

    Feat: Add Interface HTML
    Add a function to generate a html that defines the board

---
+++ html_converter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal:true
+
+require 'erb'
+
+def to_html(random_url)
+  board_template = ERB.new File.read('board.erb')
+  board = board_template.result(binding)
+
+  filename = '/var/www/src/taichi/odin-recipes/Chess/index.html'
+
+  File.open(filename, 'w') do |file|
+    file.puts board
+  end
+  puts 'Board rendered'
+end
---
+++ main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal:true
+
+load 'html_converter.rb'
+
+random_url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg'
+
+to_html(random_url)
