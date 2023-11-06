c896a2258d00bf9ea5fa17d3e7ad72f341daa3c8
RaviHelon-msf <ravihelon@gmail.com>
2023-11-06 15:25:01 -0300

    Feat: Add input
    Add input method. However it is not finished

--- piece.rb
+++ piece.rb
@@ -19,45 +19,50 @@ end
 
 # Class that defines the Horsey boy piece
 class Horse < Piece
-  def initialize(is_white)
-    super(is_white)
+  def initialize(is_white, position)
+    super(is_white, position)
     @url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg'
     @url = 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg' unless @is_white
+    @position = position
   end
 end
 
 # Class that defines the Bishy buddy piece
 class Bishop < Piece
-  def initialize(is_white)
-    super(is_white)
+  def initialize(is_white, position)
+    super(is_white, position)
     @url = 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg'
     @url = 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg' unless @is_white
+    @position = position
   end
 end
 
 # Class that defines the Rookie piece
 class Rook < Piece
-  def initialize(is_white)
-    super(is_white)
+  def initialize(is_white, position)
+    super(is_white, position)
     @url = 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg'
     @url = 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg' unless @is_white
+    @position = position
   end
 end
 
 # Class that defines the Poki piece
 class Queen < Piece
-  def initialize(is_white)
-    super(is_white)
+  def initialize(is_white, position)
+    super(is_white, position)
     @url = 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg'
     @url = 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg' unless @is_white
+    @position = position
   end
 end
 
 # Class that defines the King piece
 class King < Piece
-  def initialize(is_white)
-    super(is_white)
+  def initialize(is_white, position)
+    super(is_white, position)
     @url = 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg'
     @url = 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg' unless @is_white
+    @position = position
   end
 end
