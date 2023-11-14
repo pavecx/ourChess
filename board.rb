62dad2fa7e45ccb800006a34c38bf1db1794934c
RaviHelon-msf <ravihelon@gmail.com>
2023-11-14 12:57:15 -0300

    Feat: finished alpha version
    All the  functions necessary to play the games are in. All pieces, move interperter, interface, board

--- board.rb
+++ board.rb
@@ -5,6 +5,8 @@ load 'piece.rb'
 
 # Define the Board
 class Board
+  attr_reader :square
+
   def initialize
     @square = Array.new(8) { Array.new(8, 0) }
     @gameover = false
@@ -21,48 +23,34 @@ class Board
   end
 
   def setup
-    setup_black
-    setup_white
+    setup_pieces
     setup_pawns
     to_html
   end
 
   def setup_pawns
-    8.times do |index|
-      a = Piece.new(false, [1, index - 1])
-      @square[1][index] = a
-    end
+    setup_pawns_for_color(true, 1)
+    setup_pawns_for_color(false, 6)
+  end
 
+  def setup_pawns_for_color(is_white, row)
     8.times do |index|
-      a = Piece.new(true, [6, index - 1])
-      @square[6][index] = a
+      piece = Pawn.new(is_white, [row, index])
+      @square[row][index] = piece
     end
   end
 
-  def setup_black
-    @black_quenn_rook = Rook.new(false, [0, 0])
-    @black_quenn_knight = Horse.new(false, [0, 1])
-    @black_quenn_bishop = Bishop.new(false, [0, 2])
-    @black_queen = Queen.new(false, [0, 3])
-    @black_king = King.new(false, [0, 4])
-    @black_king_bishop = Bishop.new(false, [0, 5])
-    @black_king_knight = Horse.new(false, [0, 6])
-    @black_king_rook = Rook.new(false, [0, 7])
-    @square[0] = [@black_quenn_rook, @black_quenn_knight, @black_quenn_bishop, @black_queen,
-                  @black_king, @black_king_bishop, @black_king_knight, @black_king_rook]
+  def setup_pieces
+    setup_major_pieces(true, 1)
+    setup_major_pieces(false, 6)
   end
 
-  def setup_white
-    @white_quenn_rook = Rook.new(true, [7, 0])
-    @white_quenn_knight = Horse.new(true, [7, 1])
-    @white_quenn_bishop = Bishop.new(true, [7, 2])
-    @white_queen = Queen.new(true, [7, 3])
-    @white_king = King.new(true, [7, 4])
-    @white_king_bishop = Bishop.new(true, [7, 5])
-    @white_king_knight = Horse.new(true, [7, 6])
-    @white_king_rook = Rook.new(true, [7, 7])
-    @square[7] = [@white_quenn_rook, @white_quenn_knight, @white_quenn_bishop, @white_queen,
-                  @white_king, @white_king_bishop, @white_king_knight, @white_king_rook]
+  def setup_major_pieces(is_white, row)
+    pieces = [Rook, Horse, Bishop, Queen, King, Bishop, Horse, Rook]
+
+    pieces.each_with_index do |piece_class, index|
+      @square[row][index] = piece_class.new(is_white, [row, index])
+    end
   end
 
   def move
@@ -71,7 +59,7 @@ class Board
       msg = gets.chomp
       if msg == 'ff'
         @turn_player = !turn_player
-        return gameover
+        return @gameover = true
       end
       valid = eval_move(msg)
     end
@@ -80,7 +68,7 @@ class Board
   def eval_move(msg)
     move = msg.split('')
     move.pop if move[-1] == '+'
-    return gameover if move[-1] == '#'
+    return @gameover = true if move[-1] == '#'
 
     new_position = ['abcdefgh'.index(move[-2]), (move[-1].to_i - 1)]
     piece = 'RNBQK'.index(move[0])
@@ -91,14 +79,8 @@ class Board
     destination_piece = @square[new_position[0]][new_position[1]]
     return false if destination_piece.is_a?(Piece) && destination_piece.is_white == @turn_player
 
-    piece = select_piece(piece, new_position)
-    return selector(move) if piece.length > 1
-
-    true
-  end
-
-  def gameover
-    @gameover = true
+    move_handler = MoveHandler.new(self, piece, move, new_position, @turn_player)
+    move_handler.perform_move
   end
 
   def gameover?
@@ -115,5 +97,61 @@ class Board
       file.puts board
     end
     puts 'Board rendered'
+  end
+end
+
+# class that handles the movement
+class MoveHandler
+  def initialize(board, piece, move, new_position, turn_player)
+    @board = board
+    @move = move
+    @new_position = new_position
+    @piece = piece
+    @turn_player = turn_player
+
+    select_piece
+  end
+
+  def select_piece
+    piece_classes = [Rook, Horse, Bishop, Queen, King]
+    possibilities = piece_classes[@piece]&.move(@new_position) || move_pawn
+    @mover = filter_possibilities(possibilities)
+  end
+
+  def move_pawn
+    possibilities = [[0, -1]] unless @move.contain?('x')
+    possibilities = [[1, -1], [-1, -1]] if @move.contain?('x')
+    @turn_player ? possibilities : -possibilities
+  end
+
+  def perform_move
+    if double_move?
+      perform_double_move
+    elsif single_move?
+      perform_single_move
+    else
+      false
+    end
+  end
+
+  private
+
+  def filter_possibilities(possibilities)
+    possibilities.select do |possible|
+      @board.square[possible[0]][possible[1]].is_a?(piece_class[@new_position])
+    end
+  end
+
+  def perform_single_move
+    source_position = @mover[0]
+    destination_position = @new_position
+    move_piece(source_position, destination_position)
+  end
+
+  def move_piece(source_position, destination_position)
+    @board.square[destination_position[0]][destination_position[1]] =
+      @board.square[source_position[0]][source_position[1]]
+    @board.square[source_position[0]][source_position[1]] = ''
+    true
   end
 end
